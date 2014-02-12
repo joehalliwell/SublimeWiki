82ebb49215ed97b84786ccd3f3acce3d7665cf7b
Joe Halliwell <joe.halliwell@gmail.com>
2014-02-12 17:55:32 +0000

    Towards first-class Creole support

--- wikilink.py
+++ wikilink.py
@@ -9,39 +9,46 @@ class WikiLinkCommand(sublime_plugin.TextCommand):
         #find the cursor
         location = self.view.sel()[0]
         
-        # find the link under the cursor
-        link = self.view.substr(self.view.extract_scope(location.a)).replace("*", "")
+        # find the link text under the cursor
+        link = self.view.substr(self.view.extract_scope(location.a))
         
-        if "link.external.Wiki" in self.view.scope_name(location.a):
-                sublime.status_message("try to open " + link)
-                sublime.active_window().run_command('open_url', {"url": link})
-        elif "link.email.Wiki" in self.view.scope_name(location.a):
-                sublime.status_message("try to mail " + link)
-                sublime.active_window().run_command('open_url', {"url": "mailto:"+link})
-        elif "link.internal.Wiki" in self.view.scope_name(location.a):
-            #okay, we're good. Keep on keepin' on.        
-            
-            #compile the full file name and path.
-            new_file = os.path.join(directory, link + ".wiki")
+        if "link" not in self.view.scope_name(location.a):
+            sublime.status_message("Can only handle links!")
+            return
+        if "title" in self.view.scope_name(location.a):
+            sublime.status_message("Can't handle link titles!")
+            return
 
-            #debug section: uncomment to write to the console
-            # print("Location: %d" % location.a)
-            # print("Selected word is '%s'" % word)
-            # print("Full file path: %s" % new_file)
-            # print("Selected word link is '%s'" % self.view.scope_name(location.a))
-            # if internalLink in self.view.scope_name(location.a):
-            #     print("this is an internal link")
-            #end debug section
+        # TODO: Make a safe filename
 
-            if os.path.exists(new_file):
-                #open the already-created page.
-                new_view = window.open_file(new_file)
-            else:
-                #Create a new file and slap in the default text.
-                new_view = window.new_file()
-                default_text = "# {0}\n\nWrite about {0} here.".format(link)
-                new_view.run_command('append', {'characters': default_text})
-                new_view.set_name("%s.wiki" % link)
-                new_view.set_syntax_file("Packages/Wiki/Wiki.tmLanguage")
+        # TODO: Special cases for 
+        #sublime.status_message("try to open " + link)
+        #sublime.active_window().run_command('open_url', {"url": link})
+        #sublime.status_message("try to mail " + link)
+        #sublime.active_window().run_command('open_url', {"url": "mailto:"+link})
+        
+        #okay, we're good. Keep on keepin' on.        
+        
+        #compile the full file name and path.
+        new_file = os.path.join(directory, link + ".wiki")
+
+        #debug section: uncomment to write to the console
+        # print("Location: %d" % location.a)
+        # print("Selected word is '%s'" % word)
+        # print("Full file path: %s" % new_file)
+        # print("Selected word link is '%s'" % self.view.scope_name(location.a))
+        # if internalLink in self.view.scope_name(location.a):
+        #     print("this is an internal link")
+        #end debug section
+
+        if os.path.exists(new_file):
+            #open the already-created page.
+            new_view = window.open_file(new_file)
         else:
-            sublime.status_message("Can only open WikiWords, email addresses or web addresses.")
+            #Create a new file and slap in the default text.
+            new_view = window.new_file()
+            default_text = "# {0}\n\nWrite about {0} here.".format(link)
+            new_view.run_command('append', {'characters': default_text})
+            new_view.set_name("%s.wiki" % link)
+            new_view.set_syntax_file("Packages/Wiki/Wiki.tmLanguage")
+            
